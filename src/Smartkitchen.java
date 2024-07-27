097c4cf8c6fa42c9da4cbab253dba8310458a9a4
brian070069 <kkipngeno409@gmail.com>
2024-07-27 21:16:01 +0300

    First commit

---
+++ src/Smartkitchen.java
@@ -0,0 +1,28 @@
+public class SmartKitchen {
+    private CoffeeMaker brewmaster;
+    private Dishwasher dishwasher;
+    private Refrigerator iceBox;
+
+    public SmartKitchen(CoffeeMaker brewmaster, Dishwasher dishwasher, Refrigerator iceBox) {
+        this.brewmaster = brewmaster;
+        this.dishwasher = dishwasher;
+        this.iceBox = iceBox;
+    }
+
+    public void addWater(){
+
+    }
+    public void pourMilk(){
+
+    }
+
+    public void loadDishWasher(){
+
+    }
+
+    public void setKitchenState(bool)
+}
+
+class Refrigerator extends SmartKitchen{
+    
+}
